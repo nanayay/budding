804e9054dce060e5e63673d1d1c8d4076839b205
ayan <alexandercer@gmail.com>
2014-03-18 18:08:20 +0800

    Rename the Log Tag - rename the log tag from OGLSample to OGLESSample

--- OGLESSample_NDK/jni/log.h
+++ OGLESSample_NDK/jni/log.h
@@ -3,7 +3,7 @@
 #ifndef AY_LOG
 #define AY_LOG
 
-#define TAG "OGLSample"
+#define TAG "OGLESSample"
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
 #define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__))
 #define LGOE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__))
