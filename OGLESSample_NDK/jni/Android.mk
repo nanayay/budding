cf52dddc5d56929d3498ba62bd02cf0d2d16b291
ayan <alexandercer@gmail.com>
2014-03-18 18:12:18 +0800

    Add libpng to project - add libpng's source code and build for andorid - update the Android.mk of project

--- OGLESSample_NDK/jni/Android.mk
+++ OGLESSample_NDK/jni/Android.mk
@@ -1,14 +1,23 @@
-LOCAL_PATH := $(call my-dir)
+TOP_PATH := $(call my-dir)
 
 ##############################
+# For libpng
+include $(CLEAR_VARS)
 
+include $(TOP_PATH)/external/libpng/jni/Android.mk
+
+LOCAL_PATH := $(TOP_PATH)
+
+##############################
+# For OGLESSample_NDK
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := OGLESSample_NDK
 LOCAL_SRC_FILES := main.cpp kernel.cpp android_task.cpp android_application.cpp renderer.cpp egl_renderer.cpp timer_task.cpp renderable.cpp gl_renderable.cpp scene.cpp gl_scene.cpp android_asset.cpp
-LOCAL_LDLIBS 	:=-llog -landroid -lEGL -lGLESv2
-				  # for GLESv1, use -lGLESv1_CM
-LOCAL_STATIC_LIBRARIES	:=android_native_app_glue
+LOCAL_CFLAGS    := 
+LOCAL_LDLIBS 	:= -llog -landroid -lEGL -lGLESv2 -lz
+				  # for GLES 1.x, use -lGLESv1_CM
+LOCAL_STATIC_LIBRARIES	:= android_native_app_glue libpng
 
 include $(BUILD_SHARED_LIBRARY)
 $(call import-module, android/native_app_glue)
--- OGLESSample_NDK/jni/Application.mk
+++ OGLESSample_NDK/jni/Application.mk
@@ -1,3 +1,7 @@
-APP_ABI :=armeabi-v7a
-APP_PLATFORM :=android-18
-APP_STL	:=stlport_static
+APP_ABI := armeabi-v7a
+APP_PLATFORM := android-18
+APP_STL	:= stlport_static
+# APP_STL := gnustl_static
+# APP_OPTIM := debug 
+# APP_OPTIM := release
+# APP_ABI := armeabi armeabi-v7a
