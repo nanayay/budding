df5577a14c9f3bfb4e8e13752b8fac17827643af
ayan <alexandercer@gmail.com>
2014-03-19 18:36:45 +0800

    Bug fix of typo - typo fix for log.h

--- OGLESSample_NDK/jni/log.h
+++ OGLESSample_NDK/jni/log.h
@@ -1,11 +1,10 @@
-#include <android/log.h>
-
 #ifndef AY_LOG
 #define AY_LOG
+#include <android/log.h>
 
 #define TAG "OGLESSample"
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
 #define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__))
-#define LGOE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__))
 
 #endif
